6620de77a399c553dfc1c1b5e11f6342ade7ea3f
Lekhanapriya <96295255+Lekhanapriya@users.noreply.github.com>
2023-07-01 15:17:34 +0530

    r,c=map(int,input().split()) mat=[] for i in range(r):     b=list(map(int,input().split()))     mat.append(b) di=[] for i in range(r):     for j in range(c):         if(i==j or i+j==r-1):             di.append(mat[i][j]) print(sum(di))

---
+++ sum_of_Diagonal_values.py
@@ -0,0 +1,11 @@
+r,c=map(int,input().split())
+mat=[]
+for i in range(r):
+    b=list(map(int,input().split()))
+    mat.append(b)
+di=[]
+for i in range(r):
+    for j in range(c):
+        if(i==j or i+j==r-1):
+            di.append(mat[i][j])
+print(sum(di))
